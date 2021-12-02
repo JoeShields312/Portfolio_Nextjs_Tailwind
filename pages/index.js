48ed807f562acd1a62545c4064476003b2fcd246
Joe Shields <shields.john.joseph@gmail.com>
2021-12-01 18:46:56 -0600

    created nav bar

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import tw from 'tailwind-styled-components'
+import Nav from './components/Nav'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      Intro-Bio
-      Experience 
-      Projects 
-      Technology Used 
-      Contact Info
-    </div>
+    <Wrapper>
+      <Nav />
+      <div>Top Nav</div>
+      <div>Side Nav</div>
+      <div>Bio</div>
+      <div>Technology and Languages Used </div>
+      <div>Projects</div>
+      <div>Contact Info</div>
+      <div>Footer</div>
+    </Wrapper>
   )
 }
+
+const Wrapper = tw.div`
+felx flex-col text-center h-screen
+`
