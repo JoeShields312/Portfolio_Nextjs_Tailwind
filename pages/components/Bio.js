91459962201db2984447063e6507be023a7a96b0
Joe Shields <shields.john.joseph@gmail.com>
2021-12-03 16:45:58 -0600

    renamed hero file to bio

--- pages/components/Bio.js
+++ pages/components/Bio.js
@@ -9,9 +9,9 @@ const navigation = [
   { name: 'Company', href: '#' },
 ]
 
-export default function Hero() {
+export default function Bio() {
   return (
-    <div className="relative bg-white overflow-hidden">
+    <div className="relative bg-white overflow-scroll">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
