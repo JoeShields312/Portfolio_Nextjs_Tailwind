83fb9639d3db799c1b4a0e1a6f0d813f337a7629
Joe Shields <shields.john.joseph@gmail.com>
2021-12-03 16:12:19 -0600

    Trimmed down file

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import tw from 'tailwind-styled-components'
-import Nav from './components/Nav'
-
-
+import Head from 'next/head';
+import Image from 'next/image';
+import tw from 'tailwind-styled-components';
+import Nav from './components/Nav';
 
 export default function Home() {
-  return (
-    
-    <Wrapper>
-      <Nav>
-      <div>Bio</div>
-      <div>Technology and Languages Used </div>
-      <div>Projects</div>
-      <div>Contact Info</div>
-      <div>Footer</div>
-      </Nav>
-    </Wrapper>
-  )
+	return (
+		<Wrapper>
+			<Nav></Nav>
+		</Wrapper>
+	);
 }
 
 const Wrapper = tw.div`
 felx flex-col text-center h-screen
-`
+`;
