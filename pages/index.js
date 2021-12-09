a77207b251d399d8468d4ec224121905d1803146
Joe Shields <shields.john.joseph@gmail.com>
2021-12-08 21:30:20 -0600

    syntax fix

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,16 @@ import Image from 'next/image';
 import tw from 'tailwind-styled-components';
 import Nav from './components/Nav';
 
+
 export default function Home() {
 	return (
 		<Wrapper>
-			<Nav></Nav>
+			<Nav>
+			</Nav>
 		</Wrapper>
 	);
 }
 
 const Wrapper = tw.div`
-felx flex-col text-center h-screen
+flex flex-col text-center h-screen
 `;
