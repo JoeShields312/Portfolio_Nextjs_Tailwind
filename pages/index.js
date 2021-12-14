b8ed739284c106f3eb1396fb0a54d50628fece61
Joe Shields <shields.john.joseph@gmail.com>
2021-12-14 12:19:52 -0600

    resctructured the nav for custom pages

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,26 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import tw from 'tailwind-styled-components';
-import Nav from './components/Nav';
+import NavB from './components/NavB';
+import Footer from './components/Footer'
+import Bio from './components/Bio'
+import Projects from './projects';
+import React, { useEffect } from 'react'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 
 export default function Home() {
 	return (
 		<Wrapper>
-			<Nav>
-			</Nav>
+			<NavB>
+			</NavB>
+		
+			<Footer />
 		</Wrapper>
 	);
 }
 
 const Wrapper = tw.div`
-flex flex-col text-center h-screen
-`;
+flex flex-col text-center h-full overflow-y-auto
+`
