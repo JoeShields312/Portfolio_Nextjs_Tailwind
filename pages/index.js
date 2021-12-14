270041541e96c558af8ff7a74cb0cfa7e49ba031
Joe Shields <shields.john.joseph@gmail.com>
2021-12-14 12:25:15 -0600

    trimmed down unused imports

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,8 @@ import Image from 'next/image';
 import tw from 'tailwind-styled-components';
 import NavB from './components/NavB';
 import Footer from './components/Footer'
-import Bio from './components/Bio'
-import Projects from './projects';
 import React, { useEffect } from 'react'
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+
 
 
 export default function Home() {
