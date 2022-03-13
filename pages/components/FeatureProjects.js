485e892be3a58b3bad7f985209ac3e46a1937932
Joe Shields <shields.john.joseph@gmail.com>
2022-03-12 18:09:40 -0600

    added two projects descriptions screenshots and links

--- pages/components/FeatureProjects.js
+++ pages/components/FeatureProjects.js
@@ -1,6 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { InboxIcon, SparklesIcon } from '@heroicons/react/outline'
+import { InboxIcon, SparklesIcon, TerminalIcon } from '@heroicons/react/outline'
 import tw from 'tailwind-styled-components';
+
 
 export default function FeatureProjects() {
   return (
@@ -11,57 +12,33 @@ export default function FeatureProjects() {
             <div>
               <div>
                 <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                  <InboxIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                  <TerminalIcon className="h-6 w-6 text-white" aria-hidden="true" />
                 </span>
               </div>
               <div className="mt-6">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Stay on top of customer support
+                  Coinbase-Blockchain-Application
                 </h2>
                 <p className="mt-4 text-lg text-gray-500">
-                  Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia
-                  porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus
-                  viverra dui tellus ornare pharetra.
+                  Replica of the Coinbase application. Utilizes thirdweb, sanity for the back end, also Next.js and styled components for the front end. 
                 </p>
                 <div className="mt-6">
                   <a
-                    href="#"
+                    href="https://coinbase-blockchain-app-gules.vercel.app/"
                     className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                   >
-                    Get started
+                  Launch Site
                   </a>
                 </div>
               </div>
-            </div>
-            <div className="mt-8 border-t border-gray-200 pt-6">
-              <blockquote>
-                <div>
-                  <p className="text-base text-gray-500">
-                    &ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci risus aenean curabitur
-                    donec aliquet. Mi venenatis in euismod ut.&rdquo;
-                  </p>
-                </div>
-                <footer className="mt-3">
-                  <div className="flex items-center space-x-3">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-6 w-6 rounded-full"
-                        src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
-                        alt=""
-                      />
-                    </div>
-                    <div className="text-base font-medium text-gray-700">Marcia Hill, Digital Marketing Manager</div>
-                  </div>
-                </footer>
-              </blockquote>
             </div>
           </div>
           <div className="mt-12 sm:mt-16 lg:mt-0">
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
-                alt="Inbox user interface"
+                src="https://drive.google.com/uc?id=1n3yblcfiItBib7nZ0riGWalW8jNjHYVS"
+                alt=""
               />
             </div>
           </div>
@@ -73,24 +50,22 @@ export default function FeatureProjects() {
             <div>
               <div>
                 <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                  <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                  <TerminalIcon className="h-6 w-6 text-white" aria-hidden="true" />
                 </span>
               </div>
               <div className="mt-6">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Better understand your customers
+                  The Fade Room Barbershop
                 </h2>
                 <p className="mt-4 text-lg text-gray-500">
-                  Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia
-                  porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus
-                  viverra dui tellus ornare pharetra.
+                  The Faderoom barbershop website. Utilizes headless wordpress, next.js, tailwind, & apollo.
                 </p>
                 <div className="mt-6">
                   <a
-                    href="#"
+                    href="https://www.thefaderoominc.com/"
                     className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                   >
-                    Get started
+                    Launch Site
                   </a>
                 </div>
               </div>
@@ -100,8 +75,8 @@ export default function FeatureProjects() {
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
-                alt="Customer profile user interface"
+                src="https://drive.google.com/uc?id=1FeU8nVeR3fFVLxaSPLWq-bkOsxRk1DR7"
+                alt=""
               />
             </div>
           </div>
