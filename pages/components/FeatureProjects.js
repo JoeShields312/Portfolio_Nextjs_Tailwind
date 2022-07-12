f65b6760c9689433a5b3292a4f0e8d2ee0feb1c8
Joe Shields <shields.john.joseph@gmail.com>
2022-07-11 19:36:35 -0500

    edited projects

--- pages/components/FeatureProjects.js
+++ pages/components/FeatureProjects.js
@@ -6,47 +6,7 @@ import tw from 'tailwind-styled-components';
 export default function FeatureProjects() {
   return (
     <div className="relative bg-white pt-16 pb-32 overflow-auto overflow-x-hidden">
-
-      <div className="relative">
-        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-            <div>
-              <div>
-                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                  <TerminalIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                </span>
-              </div>
-              <div className="mt-6">
-                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Coinbase Blockchain Application
-                </h2>
-                <p className="mt-4 text-lg text-gray-500">
-                  Replica of the Coinbase application. Utilizes MetaMask, Thirdweb, Sanity.io, Next.js and styled components. *Note: You need a MetaMask Wallet to interact with this application.
-                </p>
-                <div className="mt-6">
-                  <a
-                    href="https://coinbase-blockchain-app-gules.vercel.app/"
-                    target="_blank"
-                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
-                  >
-                  Launch Site
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mt-12 sm:mt-16 lg:mt-0">
-            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-              <img
-                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://drive.google.com/uc?id=1n3yblcfiItBib7nZ0riGWalW8jNjHYVS"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-
+    
       <div className="mt-24">
         <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
           <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
@@ -58,14 +18,14 @@ export default function FeatureProjects() {
               </div>
               <div className="mt-6">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Uber Blockchain App
+                Coinbase Blockchain Application
                 </h2>
                 <p className="mt-4 text-lg text-gray-500">
-                  *Under Construction* A clone version of the Uber App that allows crypto payment. Utilizes Next.js, Ethers.js, Sanity.io, MetaMask, and Tailwind CSS. *Note: You need a MetaMask Wallet to interact with this application.
+                Replica of the Coinbase application. Utilizes MetaMask, Thirdweb, Sanity.io, Next.js and styled components. *Note: You need a MetaMask Wallet to interact with this application.
                 </p>
                 <div className="mt-6">
                   <a
-                    href="https://uber-clone-blockchain-xi-gray.vercel.app/"
+                    href="https://coinbase-blockchain-app-gules.vercel.app/"
                     target="_blank"
                     className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                   >
@@ -79,7 +39,7 @@ export default function FeatureProjects() {
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img         
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://drive.google.com/uc?id=1m6sq6eAcdwBCfosq69Z40kqDHxnsop6P"
+                src="https://drive.google.com/uc?id=1n3yblcfiItBib7nZ0riGWalW8jNjHYVS"
                 alt=""
               />
             </div>
@@ -139,14 +99,14 @@ export default function FeatureProjects() {
               </div>
               <div className="mt-6">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Inaum Salon
+                Smith Research
                 </h2>
                 <p className="mt-4 text-lg text-gray-500">
-                  Inaum Salon website, updated existing website's UI/UX and SEO.
+                Smith Research website, updated existing UI/UX and SEO.
                 </p>
                 <div className="mt-6">
                   <a
-                    href="https://www.inaumsalon.com/"
+                    href="https://www.smithresearch.com/"
                     target="_blank"
                     className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                   >
@@ -160,7 +120,7 @@ export default function FeatureProjects() {
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img         
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://drive.google.com/uc?id=1A_r9AdDt2xGGuxv6PzmRCl265zmN4t0f"
+                src="https://drive.google.com/uc?id=1XCKLpDYQG17hGk8-bUqSAn9dREng-AYX"
                 alt=""
               />
             </div>
@@ -179,14 +139,14 @@ export default function FeatureProjects() {
               </div>
               <div className="mt-6">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Smith Research
+                Weather Dashboard
                 </h2>
                 <p className="mt-4 text-lg text-gray-500">
-                  Smith Research website, updated existing UI/UX and SEO.
+                Weather Dashboard that display a 5 day weather report after searching for a location.
                 </p>
                 <div className="mt-6">
                   <a
-                    href="https://www.smithresearch.com/"
+                    href="https://weather-dashboard-mauve.vercel.app/"
                     target="_blank"
                     className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                   >
@@ -200,7 +160,7 @@ export default function FeatureProjects() {
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://drive.google.com/uc?id=1XCKLpDYQG17hGk8-bUqSAn9dREng-AYX"
+                src="https://drive.google.com/uc?id=1Xac2x1o7TWblgYXZChzPJjkquCv793dF"
                 alt=""
               />
             </div>
@@ -219,14 +179,14 @@ export default function FeatureProjects() {
               </div>
               <div className="mt-6">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Weather Dashboard
+                Avengers Quiz
                 </h2>
                 <p className="mt-4 text-lg text-gray-500">
-                  Weather Dashboard that display a 5 day weather report after searching for a location.
+                A 10 question multiple choice quiz about the Avengers, utilizes Javascript, HTML, and CSS. 
                 </p>
                 <div className="mt-6">
                   <a
-                    href="https://weather-dashboard-mauve.vercel.app/"
+                    href="https://avengers-quiz-tau.vercel.app/"
                     target="_blank"
                     className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                   >
@@ -240,46 +200,6 @@ export default function FeatureProjects() {
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img         
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://drive.google.com/uc?id=1Xac2x1o7TWblgYXZChzPJjkquCv793dF"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="relative mt-24">
-        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-            <div>
-              <div>
-                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                  <TerminalIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                </span>
-              </div>
-              <div className="mt-6">
-                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Avengers Quiz
-                </h2>
-                <p className="mt-4 text-lg text-gray-500">
-                  A 10 question multiple choice quiz about the Avengers, utilizes Javascript, HTML, and CSS. Note* This is the first website I created.
-                </p>
-                <div className="mt-6">
-                  <a
-                    href="https://avengers-quiz-tau.vercel.app/"
-                    target="_blank"
-                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
-                  >
-                  Launch Site
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mt-12 sm:mt-16 lg:mt-0">
-            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-              <img
-                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                 src="https://drive.google.com/uc?id=1T9I4R_8qlxr6fz7Qp2DmTXB2tPXjZdBm"
                 alt=""
               />
