c43258270cd2bfe4e7aca24e5f01ce78759aa0de
Joe Shields <shields.john.joseph@gmail.com>
2023-09-29 12:29:57 -0500

    updates

--- pages/components/Bio.js
+++ pages/components/Bio.js
@@ -29,7 +29,7 @@ export default function Bio() {
 							<h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
 								<span className='name block xl:inline'>Joe Shields</span>{' '}
 								<span className='block text-indigo-600 xl:inline'>
-									Full Stack Software Engineer
+									Full Stack Engineer
 								</span>
 							</h1>
 							<p className='bio break-words mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-lg lg:mx-0'>
